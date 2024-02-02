f41c44b4f8b4823d8cb08ec5aec1c0bd42c1afed
DucTin88 <nguyenductin369.com>
2024-02-02 14:52:29 +0700

    update

---
+++ src/screens/lab7/UserScreen.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "react-native-reanimated/lib/typescript/Animated";
+
+const User = () => {
+    return(
+        <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
+            <Text style={{color:'#006600', fontSize: 40}}>HomeScreen</Text>
+        </View>
+    );
+};
